d9d18cd616a891d448ff82e2eaa1bddb3fd72f52
Mateusz Hadrys <m.hadrys.95@gmail.com>
2020-07-06 16:07:21 +0200

    add error handler

--- src/templates/UserPageTemplate.js
+++ src/templates/UserPageTemplate.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
 import Header from 'components/organisms/Header/Header';
 import Footer from 'components/organisms/Footer/Footer';
+import Error from 'components/molecules/Error/Error';
 
 import bgcImage1 from 'assets/image/bgc1.svg';
 import bgcImage2 from 'assets/image/bgc2.svg';
 import bgcImage3 from 'assets/image/bgc3.svg';
+import Spinner from 'components/atoms/Spinner/Spinner';
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -37,18 +40,35 @@ const StyledBgcImage2 = styled(StyledBgcImage1)`
   left: 0;
 `;
 
-const UserPageTemplate = ({ children }) => (
+const StyledSpinner = styled(Spinner)`
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
+`;
+
+const UserPageTemplate = ({ children, loading, error }) => (
   <StyledWrapper>
     <Header />
     {children}
     <Footer />
     <StyledBgcImage1 src={bgcImage2} alt="" />
     <StyledBgcImage2 src={bgcImage3} alt="" />
+    {loading && <StyledSpinner />}
+    {error && <Error />}
   </StyledWrapper>
 );
 
 UserPageTemplate.propTypes = {
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.node]).isRequired,
+  loading: PropTypes.bool,
+  error: PropTypes.bool,
 };
 
-export default UserPageTemplate;
+UserPageTemplate.defaultProps = {
+  loading: false,
+  error: false,
+};
+
+const mapStateToProps = ({ loading, error }) => ({ loading, error });
+
+export default connect(mapStateToProps, null)(UserPageTemplate);
